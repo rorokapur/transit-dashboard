ed4168631a982c87b7f0253287c0f0788bad20eb
rorokapur <kapurkrohan@gmail.com>
2025-06-24 22:29:05 -0700

    create ArrivalItem

--- client/src/ArrivalDisplay.tsx
+++ client/src/ArrivalDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { ReactHTMLElement, useState } from 'react';
+import React, { ComponentProps, ReactHTMLElement, useState } from 'react';
 import './ArrivalDisplay.css';
 import ArrivalItem from "./ArrivalItem";
 
@@ -9,11 +9,11 @@ export interface ArrivalDisplayProps {
 const ArrivalDisplay: React.FC = (props: ArrivalDisplayProps): React.JSX.Element => {
     return (
         <div className='AD'>
-            <ArrivalItem name="2" color="grey"></ArrivalItem>
+            <ArrivalItem name="Express: UW Bothell - UW Seattle" routeId="372" color="green" status='3 min'></ArrivalItem>
             <hr></hr>
-            <ArrivalItem name="3" color="grey"></ArrivalItem>
+            <ArrivalItem name="Sand Point - East Green Lake" routeId="62" color="lime" status='1 min'></ArrivalItem>
             <hr></hr>
-            <ArrivalItem name="4" color="grey"></ArrivalItem>
+            <ArrivalItem name="Angle Lake - Lynnwood City Center" routeId="1" color="blue" status="0 min"></ArrivalItem>
         </div>
     );
 }
--- client/src/ArrivalItem.tsx
+++ client/src/ArrivalItem.tsx
@@ -2,13 +2,18 @@ import React, { ReactHTMLElement, useState } from 'react';
 import './ArrivalItem.css';
 export interface ArrivalItemProps {
     color: string;
+    routeId: string;
     name: string;
+    status: string;
 };
 
 const ArrivalItem: React.FC<ArrivalItemProps> = (props: ArrivalItemProps): React.JSX.Element => {
     return (
         <div className="AI">
-            <p>fd</p>Route {props.name}
+            <div style={{ backgroundColor: props.color }} className='route-id'><p>{props.routeId}</p></div>
+            <div className='route-name'>{props.name}</div>
+            <div className='route-status'>{props.status}</div>
+
         </div>
     );
 }
